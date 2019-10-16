147711d7cbe3944d3b2db47dec84a6428604cac3
Hdong27 <yhd943@naver.com>
2019-10-16 14:02:33 +0900

    test

--- client/app/src/main/java/com/example/runningapplication/MainActivity.kt
+++ client/app/src/main/java/com/example/runningapplication/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.runningapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_signup.setOnClickListener {
+            Toast.makeText(this.applicationContext, btn_signup.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        btn_login.setOnClickListener{
+            Toast.makeText(this.applicationContext, btn_login.text.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
