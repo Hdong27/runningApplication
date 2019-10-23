8a090130b85f866f776a5b3454026b8c6e3b314f
김현빈 <dudaduada@naver.com>
2019-10-23 15:54:45 +0900

    loginPage수정

--- client/app/src/main/java/com/example/runningapplication/LoginActivity.kt
+++ client/app/src/main/java/com/example/runningapplication/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.runningapplication
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -58,5 +59,10 @@ class LoginActivity : AppCompatActivity() {
                 }
             })
         }
+        back.setOnClickListener {
+            val landingIntent = Intent(this, LandingActivity::class.java)
+            startActivity(landingIntent)
+        }
+
     }
 }
