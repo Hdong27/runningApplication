d8fd853b0b11cc1a4b39fc5c422a1ff53b0bf9b9
임우정 <dkdleldho@nate.com>
2019-11-04 05:51:24 +0000

    Add new file

---
+++ client/app/src/main/java/com/example/runningapplication/service/UserService.kt
@@ -0,0 +1,41 @@
+package com.example.runningapplication.service
+
+import com.example.runningapplication.data.model.Challenge
+import com.example.runningapplication.data.model.User
+import retrofit2.http.*
+import retrofit2.Call
+
+interface UserService {
+
+//    @FormUrlEncoded
+    @POST("/signup.run")
+    fun signUp(
+        @Body parameters: HashMap<String,Any>
+    ):Call<Boolean>
+
+    @POST("/login.run")
+    fun login(
+        @Body parameters: HashMap<String,Any>
+    ):Call<User>
+
+    @POST("/overlap.run")
+    fun emailCheck(
+        @Body parameters: HashMap<String,Any>
+    ):Call<Boolean>
+
+
+    @GET("/findAllTeam.run")
+    fun findTeam(
+        @Query("uid") uid:Int
+    ):Call<List<Challenge>>
+
+    @GET("/findFriends.run")
+    fun findFriends(
+        @Query("email") email: String
+    ):Call<List<String>>
+
+    @POST("/friendAdd.run")
+    fun friendAdd(
+        @Body param: HashMap<String, Any>
+    ):Call<Boolean>
+}
